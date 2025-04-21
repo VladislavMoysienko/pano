8a2887ebcbc69b43d05d8c6670fe2d2732e9d736
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-21 14:11:28 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const panorama = new PANOLENS.ImagePanorama( 'images/texture_8k.png' );
+let imageContainer = document.querySelector('.image-container')
+
+
+var infospotPositions = [
+    new THREE.Vector3(-2136.06, 16.30, 890.14),
+    new THREE.Vector3(-3136.06, 296.30, -4290.14),
+
+  ];
+
+const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    autoRotate: true,
+    autoRotateSpeed: 0.3,
+    controlBar: true,
+});
+
+viewer.add( panorama );
