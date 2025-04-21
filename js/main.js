400941409d855be87f1a1f3cbbcf61f88840d9df
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-21 15:19:01 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
-let imageContainer = document.querySelector('.image-container');
+const panorama = new PANOLENS.ImagePanorama( 'images/texture_8k.png' );
+let imageContainer = document.querySelector('.image-container')
 
-const verticalFOVDeg = (1665 / 8580) * 360;
-const vStartDeg = (180 - verticalFOVDeg) / 2;
-const vEndDeg = verticalFOVDeg;
 
-const geometry = new THREE.SphereBufferGeometry(
-  5000,
-  60,
-  40,
-  0,
-  Math.PI * 2,
-  THREE.Math.degToRad(vStartDeg),
-  THREE.Math.degToRad(vEndDeg) 
-);
+var infospotPositions = [
+    // new THREE.Vector3(-2136.06, 16.30, 890.14),
+    // new THREE.Vector3(-3136.06, 296.30, -4290.14),
 
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('images/texture_8k.png');
-
-const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
-const mesh = new THREE.Mesh(geometry, material);
-
-const panorama = new PANOLENS.ImagePanorama('images/texture_8k.png');
-panorama.add(mesh);
+  ];
 
 const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  controlBar: true,
-  horizontalView: true,
+    container: imageContainer,
+    horizontalView: true,
+    controlBar: true,
 });
 
-viewer.camera.fov = 75;
-viewer.camera.updateProjectionMatrix();
-
-viewer.add(panorama);
+viewer.add( panorama );
