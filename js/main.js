46dac74f65bc5cf93a8b1363cc1514c1fcfb2cde
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-21 17:09:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-const panorama = new PANOLENS.ImagePanorama( 'images/texture_8k.png' );
+const panorama = new PANOLENS.ImagePanorama('images/texture_8k.png');
 let imageContainer = document.querySelector('.image-container')
 
 
-var infospotPositions = [
-    // new THREE.Vector3(-2136.06, 16.30, 890.14),
-    // new THREE.Vector3(-3136.06, 296.30, -4290.14),
-
-  ];
+var infospot;
+infospot = new PANOLENS.Infospot();
+infospot.position.set( 5000.00, -665.23, -3996.49 );
+infospot.addHoverText('Test');
 
 const viewer = new PANOLENS.Viewer({
     container: imageContainer,
     horizontalView: true,
     controlBar: true,
 });
+panorama.setLinkingImage('images/IMG_0850.png')
 
-viewer.add( panorama );
-viewer.control.fov = 65;
-viewer.control.maxFov = 65;
-viewer.control.minFov = 65;
+viewer.add(panorama, infospot);
+viewer.control.maxFov = 60;
+viewer.control.minFov = 60;
