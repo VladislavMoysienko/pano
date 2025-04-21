3610ac19f2dfa171d69546e25b0f2c952c963787
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-21 14:42:52 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ let imageContainer = document.querySelector('.image-container')
 
 
 var infospotPositions = [
-    new THREE.Vector3(-2136.06, 16.30, 890.14),
-    new THREE.Vector3(-3136.06, 296.30, -4290.14),
+    // new THREE.Vector3(-2136.06, 16.30, 890.14),
+    // new THREE.Vector3(-3136.06, 296.30, -4290.14),
 
   ];
 
 const viewer = new PANOLENS.Viewer({
     container: imageContainer,
-    autoRotate: true,
-    autoRotateSpeed: 0.3,
+    horizontalView: true,
     controlBar: true,
 });
 
 viewer.add( panorama );
+viewer.cameraControls.minPolarAngle = THREE.MathUtils.degToRad(45);
+viewer.cameraControls.maxPolarAngle = THREE.MathUtils.degToRad(135);
