9f2c2774d4dd611418ed620bbbc119ef097eda02
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-21 17:20:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let imageContainer = document.querySelector('.image-container')
 
 var infospot;
 infospot = new PANOLENS.Infospot();
-infospot.position.set( 5000.00, -665.23, -3996.49 );
+infospot.position.set( -5000.00, -1825.25, 197.56 );
 infospot.addHoverText('Test');
 
 const viewer = new PANOLENS.Viewer({
@@ -14,6 +14,7 @@ const viewer = new PANOLENS.Viewer({
 });
 panorama.setLinkingImage('images/IMG_0850.png')
 
-viewer.add(panorama, infospot);
+panorama.add(infospot);
+viewer.add(panorama);
 viewer.control.maxFov = 60;
 viewer.control.minFov = 60;
