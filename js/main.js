5182f6de20a138432574e6acc7fab54022a006b9
VladislavMoysienko <131446130+VladislavMoysienko@users.noreply.github.com>
2025-04-22 09:57:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,14 +4,6 @@ let imageContainer = document.querySelector('.image-container')
 
 var infospot = new PANOLENS.Infospot();
 infospot.position.set( 5000.00, 300, 1500 );
-infospot.hoverText(`Час преблаженный,
-День вожделенный!
-Мы оставляем,
-Мы покидаем
-Илимские горы,
-Берлоги, норы.
-А.Н.Радищев`);
-
 const viewer = new PANOLENS.Viewer({
     container: imageContainer,
     horizontalView: true,
